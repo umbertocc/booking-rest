bf83fe69f7677ff38240ba1b41e4e020e5856ee7
Umberto <xesi09@informatica.aci.it>
2026-04-22 23:49:00 +0200

    feat

--- src/main/java/com/example/mail/DemoMailApplication.java
+++ src/main/java/com/example/mail/DemoMailApplication.java
@@ -2,8 +2,10 @@ package com.example.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.mail")
 public class DemoMailApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoMailApplication.class, args);
